47ca62a9a45cac58208058beeff355cd2577d331
Khoi Pham <khoi.drca@gmail.com>
2022-10-04 18:14:59 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//Load express module with `require` directive
+var express = require('express')
+var app = express()
+//Define request response in roor URL (/)
+app.get('/a', function (req, res) {
+ res.send('Hello World! main branch')
+ })
+//Launch listening server on port 8081
+app.listen(8081, function () {
+  console.log('app listening on port 8081!')
+})
