489c5a2b40736066712a62fca2f17f856822d0b9
Khoi Pham <khoi.drca@gmail.com>
2022-10-05 10:07:12 +0700

    f3 is now f4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express()
 //Define request response in roor URL (/)
 app.get('/a', function (req, res) {
- res.send('Hello World! F3 F4')
+ res.send('Hello World! F3 is now F4')
  })
 //Launch listening server on port 8081
 app.listen(8081, function () {
